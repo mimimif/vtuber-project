c03271fbe4d3f08e50bd00f08b224797d07cd2dd
mimimi <takumaf3@gmail.com>
2026-02-21 01:49:47 +0900

    Initialize Realtime Motion Engine project skeleton (C++)

---
+++ src/motion_engine/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "VTuber Realtime Motion Engine Simulator started." << std::endl;
+    // TODO: Initialize motion capture input (e.g., OpenCV, Leap Motion SDK)
+    // TODO: Setup WebSocket server to stream bone rotation data to OBS / Avatar Builder
+    
+    return 0;
+}
